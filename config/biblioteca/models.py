681b4e5f1249e45c3d0010b810f6b06dde05cd00
cettipao <cettipao@live.com.ar>
2020-05-14 09:53:10 -0300

    Modelo Libro en biblioteca

--- config/biblioteca/models.py
+++ config/biblioteca/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Autor (models.Model):
+class Libro(models.Model):
+    titulo = models.CharField(max_length = 30,null=False)
+    editorial = models.CharField(max_length = 30,null=False)
+    paginas = models.IntegerField(null=False)
+
+class Autor(models.Model):
     nombre = models.CharField(max_length = 30,null=False)
+    def __str__(self):
+        return str(self.nombre)
