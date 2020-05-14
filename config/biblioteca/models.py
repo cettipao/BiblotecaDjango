4f6cd0f4c324a5bb120be8af4cd47d375c6f3f82
cettipao <cettipao@live.com.ar>
2020-05-14 10:00:32 -0300

    Modelo Ejemplar en bibloteca, override de toStrings y algunas relaciones

--- config/biblioteca/models.py
+++ config/biblioteca/models.py
@@ -2,10 +2,29 @@ from django.db import models
 
 # Create your models here.
 
+
+
+class Ejemplar(models.Model):
+    libro = models.ForeignKey(
+        'Libro',
+        on_delete=models.CASCADE,
+        null=False
+    )
+    localizacion = models.CharField(max_length = 30,null=False)
+    def __str__(self):
+        return str("{} de {}".format(self.localizacion,self.libro.titulo))
+
 class Libro(models.Model):
     titulo = models.CharField(max_length = 30,null=False)
     editorial = models.CharField(max_length = 30,null=False)
     paginas = models.IntegerField(null=False)
+    autor = models.ForeignKey(
+        'Autor',
+        on_delete=models.CASCADE,
+        null=False
+    )
+    def __str__(self):
+        return str(self.titulo)
 
 class Autor(models.Model):
     nombre = models.CharField(max_length = 30,null=False)
