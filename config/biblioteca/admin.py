2b0a89481c59d2df9bda0632b9142da93d408b93
cettipao <cettipao@live.com.ar>
2020-05-28 13:56:00 -0300

    Django Suit (Theme para el aadmin) agregado

--- config/biblioteca/admin.py
+++ config/biblioteca/admin.py
@@ -1,10 +1,22 @@
+from django.forms import ModelForm, TextInput
+from django.contrib.admin import ModelAdmin
 from django.contrib import admin
 
 # Register your models here.
 
 from .models import *
 
+class LibroForm(ModelForm):
+    class Meta:
+        widgets = {
+            'titulo': TextInput(attrs={'class': 'input-mini'})
+        }
+        
+class LibroAdmin(ModelAdmin):
+    form = LibroForm
+
+
 admin.site.register(Autor)
-admin.site.register(Libro)
+admin.site.register(Libro,LibroAdmin)
 admin.site.register(Usuario)
 admin.site.register(Ejemplar)
